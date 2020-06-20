5d3e657510b8e494ec90bfa4d9ba46fd05e3846e
Ray Haleblian <ray@haleblian.com>
2020-06-19 21:38:18 -0400

    clang-format.

--- src/cli.h
+++ src/cli.h
@@ -2,23 +2,23 @@
 #pragma once
 
 typedef struct {
-  char *enablelogging;
-  char *fullscreen;
-  char *boot;
-  char *benchmark;
-  char *conffile;
-  char *hardfile;
-  char *imagefile1;
-  char *imagefile2;
-  char *statefile;
+    char *enablelogging;
+    char *fullscreen;
+    char *boot;
+    char *benchmark;
+    char *conffile;
+    char *hardfile;
+    char *imagefile1;
+    char *imagefile2;
+    char *statefile;
 } cli_t;
 
 enum cli_error_code {
-  ERROR_NONE = 0,
-  ERROR_UNKNOWN_OPTION = 1,
-  ERROR_UNKNOWN_OPTARG = 2,
-  ERROR_UNKNOWN_FILETYPE = 4,
-  ERROR_USAGE = 128
+    ERROR_NONE = 0,
+    ERROR_UNKNOWN_OPTION = 1,
+    ERROR_UNKNOWN_OPTARG = 2,
+    ERROR_UNKNOWN_FILETYPE = 4,
+    ERROR_USAGE = 128
 };
 
 int parseCommandLine(int argc, char *argv[], cli_t *cli);
